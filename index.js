2b3b16607777c499597485610e3dd0a59545a7f8
Breyner Parada <paradabre@gmail.com>
2024-03-14 18:05:12 -0500

    Index.js updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = config.port;
 main();
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use("/app", express.static("public", "index.html"));
+app.use("/app", express.static("public"));
 app.use(cors());
 routerWeb(app);
 
