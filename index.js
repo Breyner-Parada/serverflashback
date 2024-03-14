821da41e398cf9aaf283db388185dfbd1fc61200
Breyner Parada <paradabre@gmail.com>
2024-03-14 17:34:54 -0500

    express static updated

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import { main } from "./lib/mongo.js";
 import { routerWeb } from "./routes/index.js";
 import { config } from "./config/index.js";
-import path from "path";
 
 const app = express();
 const port = config.port;
@@ -11,7 +10,7 @@ const port = config.port;
 main();
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(express.static(path.join(__dirname, "public", "index.html")));
+app.use("/app", express.static("public", "index.html"));
 app.use(cors());
 routerWeb(app);
 
